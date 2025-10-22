8bc297f9491058e7258b3b61731932c883b6107b
AmmarHammamieh <72897926+AmmarHammamieh@users.noreply.github.com>
2025-10-22 14:24:20 +0300

    Update product.js

--- product.js
+++ product.js
@@ -601,7 +601,7 @@ async function initializePageContent() {
     let family_id = params.get("family_id")?.replaceAll('"', '');
     const urls = [
         base_url+`/Economic_product/get_product_families/${family_id}`,
-        base_url+`/Economic_product/get_economic_product_collections/${family_id}?is_published=1`
+        base_url+`/Economic_product/get_economic_product_collections?product_family_id=${family_id}&is_published=1`
     ];
     const responses = await Promise.all(
       urls.map(url =>
@@ -709,6 +709,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
 
 
 
